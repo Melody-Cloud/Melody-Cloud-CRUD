ce8482cfa42bf8e2f2bfac8bcfa97acf56dc7040
Szymon Sadowski <szymonsadowski3@gmail.com>
2018-11-07 23:25:03 +0100

    - implemented models

--- models.py
+++ models.py
@@ -1,6 +1,6 @@
 import datetime
 
-from peewee import TextField, DateTimeField, Model
+from peewee import TextField, DateTimeField, Model, CharField, ForeignKeyField, IntegerField
 
 from persistence import pgdb
 
@@ -11,9 +11,46 @@ class BaseModel(Model):
         database = pgdb
 
 
-class Message(BaseModel):
-    content = TextField()
-    pub_date = DateTimeField(default=datetime.datetime.now)
+class Artist(BaseModel):
+    name = CharField()
+    artistDescription = TextField()
+    createdDate = DateTimeField(default=datetime.datetime.now)
 
 
-pgdb.create_tables([Message])
+class Album(BaseModel):
+    artistId = ForeignKeyField(Artist, backref='albums')
+    albumName = CharField()
+    albumDescription = TextField()
+    createdDate = DateTimeField(default=datetime.datetime.now)
+
+
+class Song(BaseModel):
+    artistId = ForeignKeyField(Artist, backref='songs')
+    albumId = ForeignKeyField(Album, backref='tags')
+    name = CharField()
+    musicSrcUrl = CharField()
+    waveformImgUrl = CharField()
+    amountOfPlays = IntegerField()
+    amountOfLike = IntegerField()
+    description = TextField()
+    createdDate = DateTimeField(default=datetime.datetime.now)
+
+
+class Tag(BaseModel):
+    songId = ForeignKeyField(Song, backref='tags')
+    songTag = CharField()
+    createdDate = DateTimeField(default=datetime.datetime.now)
+
+
+class Comment(BaseModel):
+    songId = ForeignKeyField(Song, backref='comments')
+    commentContent = TextField()
+    createdDate = DateTimeField(default=datetime.datetime.now)
+
+
+class Playlist(BaseModel):
+    playlistName = CharField()
+    songId = ForeignKeyField(Song, backref='songs')
+
+
+pgdb.create_tables([Song, Tag, Comment, Artist, Album, Playlist])
---
+++ populate.py
@@ -0,0 +1,4 @@
+from models import Artist
+
+peep = Artist(name='Lil Peep', artistDescription='Goth Angel Sinner.')
+peep.save()
